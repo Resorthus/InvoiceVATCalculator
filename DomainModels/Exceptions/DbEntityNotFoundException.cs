b0e5e07dbaf491d696b018411a973ce815499f1e
Martis <martynas.maslauskas@gmail.com>
2022-11-15 00:37:08 +0200

    Re-added DomainModels project

---
+++ DomainModels/Exceptions/DbEntityNotFoundException.cs
@@ -0,0 +1,18 @@
+﻿using DomainModels.Entities;
+using System.Net;
+
+namespace DomainModels.Exceptions
+{
+    public class DbEntityNotFoundException<T> : Exception 
+        where T : Entity
+    {
+        public HttpStatusCode StatusCode = HttpStatusCode.NotFound;
+
+        public string Message { get; set; }
+
+        public DbEntityNotFoundException(int id)
+        {
+            Message = $"{typeof(T).Name} entry {id} not found";
+        }
+    }
+}
