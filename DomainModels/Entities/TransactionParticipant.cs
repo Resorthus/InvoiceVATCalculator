53037935fcb21a1b7a2c5365080ad8ff8e4e6c84
Martis <martynas.maslauskas@gmail.com>
2022-11-14 19:33:52 +0200

    Added entity models and enums

---
+++ DomainModels/Entities/TransactionParticipant.cs
@@ -0,0 +1,26 @@
+﻿using DomainModels.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainModels.Entities
+{
+    public abstract class TransactionParticipant : Entity
+    {
+        public string Name { get; set; }
+
+        public PersonType Type { get; set; }
+
+        public bool IsVATApplicable { get; set; }
+
+        public int CountryId { get; set; }
+
+        public Country Country { get; set; }
+
+        public virtual ICollection<Invoice> 
+
+        public TransactionParticipant() { }
+    }
+}
---
+++ DomainModels/Enums/Continent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainModels.Enums
+{
+    public enum Continent
+    {
+        Europe,
+        Asia,
+        Africa,
+        Australia,
+        NorthAmerica,
+        SouthAmerica,
+        Antarctica,
+    }
+}
